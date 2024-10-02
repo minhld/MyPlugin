80815f45899b3ab1d5fd49278c8171cb81904c59
minhld <minhld38@gmail.com>
2024-10-01 23:15:09 -0500

    added plugin configs

---
+++ src/main/java/com/minhle/tools/myplugin/AskQuestionAction.java
@@ -0,0 +1,34 @@
+package com.minhle.tools.myplugin;
+
+import com.intellij.ide.BrowserUtil;
+import com.intellij.lang.Language;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.editor.CaretModel;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.psi.PsiFile;
+import org.jetbrains.annotations.NotNull;
+
+public class AskQuestionAction extends AnAction {
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent e) {
+        PsiFile file = e.getData(CommonDataKeys.PSI_FILE);
+        Language lang = e.getData(CommonDataKeys.PSI_FILE).getLanguage();
+        String languageTag = "+[" + lang.getDisplayName().toLowerCase() + "]";
+
+        Editor editor = e.getRequiredData(CommonDataKeys.EDITOR);
+        CaretModel caretModel = editor.getCaretModel();
+        String selectedText = caretModel.getCurrentCaret().getSelectedText();
+
+        String query = selectedText.replace(' ', '+') + languageTag;
+        BrowserUtil.browse("https://stackoverflow.com/questions/ask");
+    }
+
+    @Override
+    public void update(@NotNull AnActionEvent e) {
+        Editor editor = e.getRequiredData(CommonDataKeys.EDITOR);
+        CaretModel caretModel = editor.getCaretModel();
+        e.getPresentation().setEnabledAndVisible(caretModel.getCurrentCaret().hasSelection());
+    }
+}
